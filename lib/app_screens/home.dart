ae2512d3989b678803954d27ce92b6d4f1ac2cf9
erikrios <erikriosetiawan49@gmail.com>
2020-02-07 18:59:48 +0700

    Add Row Widget

--- lib/app_screens/home.dart
+++ lib/app_screens/home.dart
@@ -5,19 +5,32 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Center(
       child: Container(
-        alignment: Alignment.center,
-        color: Colors.deepPurple,
-        child: Text(
-          'Flight',
-          textDirection: TextDirection.ltr,
-          style: TextStyle(
-              decoration: TextDecoration.none,
-              fontSize: 75.0,
-              fontFamily: 'Raleway',
-              fontWeight: FontWeight.w700,
-              color: Colors.white),
-        ),
-      ),
+          alignment: Alignment.center,
+          color: Colors.deepPurple,
+          child: Row(
+            children: <Widget>[
+              Text(
+                'Spice Jet',
+                textDirection: TextDirection.ltr,
+                style: TextStyle(
+                    decoration: TextDecoration.none,
+                    fontSize: 35.0,
+                    fontFamily: 'Raleway',
+                    fontWeight: FontWeight.w700,
+                    color: Colors.white),
+              ),
+              Text(
+                'From Mumbai to Bangalore via New Delhi',
+                textDirection: TextDirection.ltr,
+                style: TextStyle(
+                    decoration: TextDecoration.none,
+                    fontSize: 35.0,
+                    fontFamily: 'Raleway',
+                    fontWeight: FontWeight.w700,
+                    color: Colors.white),
+              )
+            ],
+          )),
     );
   }
 }
